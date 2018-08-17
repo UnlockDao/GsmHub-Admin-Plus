f7cbec2206648d3f645b8fdd52f4af89578acc2d
binhnguyen1998822 <32400888+binhnguyen1998822@users.noreply.github.com>
2018-08-17 22:30:30 +0700

    table color

--- resources/views/imeiservice.blade.php
+++ resources/views/imeiservice.blade.php
@@ -20,8 +20,8 @@
 
                         </div>
                         <div class="card-body">
-                            <div class="table-responsive">
-                                <table id="testTable" class="table">
+                            <div class="table-responsive table-full-width table-hover">
+                                <table id="testTable" class="table table-striped">
                                     <thead class="text-primary">
                                     <th width="2%"></th>
                                     <th>Service Name</th>
@@ -38,13 +38,13 @@
                                     </thead>
                                     <tbody>
                                     @foreach($group as $g)
-                                        <tr><td><i class="material-icons">monetization_on</i></td><td><strong style="font-weight:700;">{{$g->group_name}}</strong></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+                                        <tr class="table-warning"><td><i class="material-icons">monetization_on</i></td><td><strong style="font-weight:700;">{{$g->group_name}}</strong></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
                                     @foreach($imei_service as $v)
                                         @if($v->imei->imei_service_group_id == $g->id)
                                             <tr id="{{$v->id_imei}}">
                                                 <td>{{$v->id_imei}}</td>
                                                 <td @if($v->imei->status == 'soft_deleted' )style="text-decoration: line-through;"@endif>{{$v->imei->service_name}}</td>
-                                                <td>@if($v->imei->pricefromapi == 1) API @else Manual @endif</td>
+                                                <td>@if($v->imei->pricefromapi == 1)<span class="badge badge-pill badge-success">API<span>  @else<span class="badge badge-pill badge-info">Manual<span>  @endif</td>
                                                 <td>@if($v->nhacungcap ==! null){{$v->nhacungcap->name}}@endif</td>
                                                 @if($v->imei->pricefromapi == 1)
                                                     <td><?php echo number_format($v->imei->api_credit,2); ?></td>
