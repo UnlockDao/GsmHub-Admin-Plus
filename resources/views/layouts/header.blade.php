aad9ff43669c44cf3526a35b5f9b4e254162d7aa
ManhIT <manhit96@gmail.com>
2018-08-19 12:57:55 +0700

    fix ssl

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -9,8 +9,8 @@
     <title>Kế Toán</title>
     <link href="{{ asset('assets/css/material-dashboard.min.css?v=2.0.1') }}" rel="stylesheet"/>
     <!--     Fonts and icons     -->
-    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
-    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300|Material+Icons' rel='stylesheet'
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,700,300|Material+Icons' rel='stylesheet'
           type='text/css'>
     <script src="{{ asset('excel/tableToExcel.js') }}"></script>
 </head>
