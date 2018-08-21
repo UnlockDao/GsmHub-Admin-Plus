a831978d157ad0f069279a9a7559ca565b642797
Binh Nguyen <binhnguyen199882@gmail.com>
2018-08-21 14:54:15 +0700

    update supplier

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -43,11 +43,11 @@ class SupplierController extends Controller
         $imeiprice = Imeiservicepricing::where('id_nhacungcap',$id)->get();
         foreach ($imeiprice as $i){
             if($i->gianhap ==! null){
-            $giaphi = ($request->tigia * $i->gianhap) / 22000 + (($i->gianhap / 100) * $request->phi);
-            $updategiaphi = Imeiservice::where('id', $i->id)->update(['purchase_cost' => $giaphi]);
+                $giaphi = ($request->tigia * $i->gianhap) / 22000 + (($i->gianhap / 100) * $request->phi);
+                $updategiaphi = Imeiservice::where('id', $i->id)->update(['purchase_cost' => $giaphi]);
             }
         }
-        
+        $this->update();
         return redirect('/supplier');
     }
 
@@ -57,23 +57,24 @@ class SupplierController extends Controller
         $imei = Imeiservice::get();
         $client = Clientgroup::get();
 
-            foreach ($imei as $i){
-                $imeiprices = Clientgroupprice::where('service_id',$i->id)->where('group_id','19')->where('currency','USD')->first();
-                    if($imeiprices ==! null){
-                        $giabanle = $i->credit+$imeiprices->discount;
-                        foreach ($client as $c){
-                            $chietkhau = ($giabanle - ((($giabanle - $i->purchase_cost) / 100) *$c->chietkhau));
-                            $y = $chietkhau-$i->credit;
-                            foreach ($currencies as $cu) {
-                                $updatepriceuse = Clientgroupprice::where('group_id', $c->id)
-                                    ->where('service_type', 'imei')
-                                    ->where('currency', $cu->currency_code)
-                                    ->where('service_id', $i->id)
-                                    ->update(['discount' => $y * $c->exchange_rate_static]);
-                            }
+        foreach ($imei as $i){
+            $imeiprices = Clientgroupprice::where('service_id',$i->id)->where('group_id','19')->where('currency','USD')->first();
+            if($imeiprices ==! null){
+                $giabanle = $i->credit+$imeiprices->discount;
+                foreach ($client as $c){
+                    $chietkhau = ($giabanle - ((($giabanle - $i->purchase_cost) / 100) *$c->chietkhau));
+                    $y = $chietkhau-$i->credit;
+                    foreach ($currencies as $cu) {
+                        $b= $y*$cu->exchange_rate_static;
+                        $updatepriceuse = Clientgroupprice::where('group_id', $c->id)
+                            ->where('service_type', 'imei')
+                            ->where('currency', $cu->currency_code)
+                            ->where('service_id', $i->id)
+                            ->update(['discount' => $b]);
                     }
                 }
             }
+        }
         return;
     }
 }
