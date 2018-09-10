5040f144a83d3713a10a06e89513563daefdc106
Binh Nguyen <binhnguyen199882@gmail.com>
2018-09-10 16:16:20 +0700

    shortcut icon

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="https://s-unlock.com/default/images/header/favicon/favicon.ico">
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Pricing | S-Unlock</title>
