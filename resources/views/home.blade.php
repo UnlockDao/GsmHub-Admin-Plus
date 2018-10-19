739534e488d701f3f59155ae1e59b74181693230
To Nguyen <32400888+binhnguyen1998822@users.noreply.github.com>
2018-10-19 11:30:17 +0700

    load js in local

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -372,12 +372,9 @@
         </div>
 
     </div>
-        <script src="https://code.highcharts.com/highcharts.js"></script>
-        <script src="https://code.highcharts.com/modules/exporting.js"></script>
-        <script src="https://code.highcharts.com/modules/export-data.js"></script>
 
 
-
+        <script src="{{ asset('js/highcharts.js') }}"></script>
 <script>
 
     Highcharts.chart('container', {
