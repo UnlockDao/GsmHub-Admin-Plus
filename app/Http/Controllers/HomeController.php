1d24ec0a74a5dc1c53ba10ea0109b98a36447afd
binhnguyen1998822 <32400888+binhnguyen1998822@users.noreply.github.com>
2018-11-05 17:09:39 +0700

    Optimal Code Chart

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\CUtil;
 use App\Imeiserviceorder;
 use App\Serverserviceorder;
+use DateTime;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -27,46 +28,6 @@ class HomeController extends Controller
      */
     public function index(Request $request)
     {
-        $day1 = CUtil::convertDateS(date('Y-m-d'));
-        $day2 = CUtil::convertDateS(date('Y-m-d H:i:s'));
-
-        $yesterday1 = CUtil::convertDateS(date('Y-m-d', strtotime('-1 days')));
-        $yesterday2 = CUtil::convertDateS(date('Y-m-d 23:59:59', strtotime('-1 days')));
-
-        $week1 = CUtil::convertDateS(date('Y-m-d', strtotime('-7 days')));
-        $week2 = CUtil::convertDateS(date('Y-m-d 23:59:59', strtotime('-7 days')));
-
-        $month1 = CUtil::convertDateS(date('Y-m-1'));
-        $month2 = CUtil::convertDateS(date('Y-m-d 23:59:59'));
-
-
-        $serveroderday = Serverserviceorder::where('status', 'COMPLETED')->whereBetween('completed_on', [$day1, $day2])
-            ->select(DB::raw('SUM(credit_default_currency-(purchase_cost*quantity)) as profit'))
-            ->first();
-        $serveroderyesterday = Serverserviceorder::where('ignore_profit', 0)->where('status', 'COMPLETED')->whereBetween('completed_on', [$yesterday1, $yesterday2])
-            ->select(DB::raw('SUM(credit_default_currency-(purchase_cost*quantity)) as profit'))
-            ->first();
-        $serveroderweek = Serverserviceorder::where('ignore_profit', 0)->where('status', 'COMPLETED')->whereBetween('completed_on', [$week1, $week2])
-            ->select(DB::raw('SUM(credit_default_currency-(purchase_cost*quantity)) as profit'))
-            ->first();
-        $serverodermonth = Serverserviceorder::where('ignore_profit', 0)->where('status', 'COMPLETED')->whereBetween('completed_on', [$month1, $month2])
-            ->select(DB::raw('SUM(credit_default_currency-(purchase_cost*quantity)) as profit'))
-            ->first();
-
-
-        $imeioderday = Imeiserviceorder::where('ignore_profit', 0)->where('status', 'COMPLETED')->whereBetween('completed_on', [$day1, $day2])
-            ->select(DB::raw('SUM(credit_default_currency-purchase_cost) as profit'))
-            ->first();
-        $imeioderyesterday = Imeiserviceorder::where('ignore_profit', 0)->where('status', 'COMPLETED')->whereBetween('completed_on', [$yesterday1, $yesterday2])
-            ->select(DB::raw('SUM(credit_default_currency-purchase_cost) as profit'))
-            ->first();
-        $imeioderweek = Imeiserviceorder::where('ignore_profit', 0)->where('status', 'COMPLETED')->whereBetween('completed_on', [$week1, $week2])
-            ->select(DB::raw('SUM(credit_default_currency-purchase_cost) as profit'))
-            ->first();
-        $imeiodermonth = Imeiserviceorder::where('ignore_profit', 0)->where('status', 'COMPLETED')->whereBetween('completed_on', [$month1, $month2])
-            ->select(DB::raw('SUM(credit_default_currency-purchase_cost) as profit'))
-            ->first();
-
         $datefilter = $request->datefilter;
         $tg = explode(" - ", $datefilter);
         if ($datefilter == null) {
@@ -94,93 +55,40 @@ class HomeController extends Controller
             ->where('credit_default_currency', '!=', 0)
             ->selectRaw('sum(credit_default_currency - ( purchase_cost * IF( quantity >0, quantity, 1 ) ) ) as profit')
             ->first();
-
-
+        //get day in month
+        $date = DateTime::createFromFormat("Y-n", "2018-11");
+        $datesArray = array();
+        for ($i = 1; $i <= $date->format("t"); $i++) {
+            $datesArray[] = DateTime::createFromFormat("Y-n-d", "2018-11-$i")->format('d-n-Y');
+        }
         //server
-        $serverchart2 = Serverserviceorder::where('ignore_profit', 0)
-            // ->where('purchase_cost', '>', 0) // wrong condition
+        $serverchart = Serverserviceorder::where('ignore_profit', 0)
             ->orderBy('completed_on')
             ->where('credit_default_currency', '!=', 0)
             ->where('status', '=', 'Completed')
             ->where('amount_debitted', '=', 1)
             ->whereBetween('completed_on', [$tg1, $tg2])
-            ->get();
-        if(!$serverchart2->isEmpty()){
-        foreach ($serverchart2 as $s) {
-            $data_array[] =
-                array(
-                    'completed_on' => CUtil::convertDate($s->completed_on, 'd-m-Y'),
-                    'credit_default_currency' => $s->credit_default_currency,
-                    'purchase_cost' => $s->purchase_cost,
-                    'quantity' => $s->quantity,
-                    'profit' => $s->credit_default_currency - ($s->purchase_cost * $s->quantity)
-                );
-        }
-        $collectserver = collect($data_array);
-        $chartserver = $collectserver->groupBy('completed_on')->map(function ($item) {
-            return $item->sum(function ($item) {
-                return (number_format($item['profit']));
-            });
-        })
-            ->toArray();
-        //imei
-        $imeichart2 = Imeiserviceorder::where('ignore_profit', 0)
-            // ->where('purchase_cost', '>', 0) // wrong condition
-            ->orderBy('completed_on')
+            ->groupBy('date')
+            ->orderBy('date', 'ASC')
+            ->get([
+                DB::raw('date(convert_tz(completed_on,"+00:00","+07:00")) as date'),
+                DB::raw('COUNT(*) as value'),
+                DB::raw('sum(credit_default_currency - ( purchase_cost * IF( quantity >0, quantity, 1 ) ) ) as profit')
+            ]);
+
+        $imeichart = Imeiserviceorder::where('ignore_profit', 0)
             ->where('credit_default_currency', '!=', 0)
             ->where('status', '=', 'Completed')
             ->where('amount_debitted', '=', 1)
             ->whereBetween('completed_on', [$tg1, $tg2])
-            ->get();
-        foreach ($imeichart2 as $s) {
-            $data_arrayi[] =
-                array(
-                    'completed_on' => CUtil::convertDate($s->completed_on, 'd-m-Y'),
-                    'credit_default_currency' => $s->credit_default_currency,
-                    'purchase_cost' => $s->purchase_cost,
-                    'profit' => $s->credit_default_currency - $s->purchase_cost
-                );
-        }
-        $collectimei = collect($data_arrayi);
-        $chartimei = $collectimei->groupBy('completed_on')->map(function ($item) {
-            return $item->sum(function ($item) {
-                return (number_format($item['profit']));
-            });
-        })
-            ->toArray();
+            ->groupBy('date')
+            ->orderBy('date', 'ASC')
+            ->get([
+                DB::raw('date(convert_tz(completed_on,"+00:00","+07:00")) as date'),
+                DB::raw('COUNT(*) as value'),
+                DB::raw('sum(if(link_order_id != 0, credit_default_currency, credit_default_currency - purchase_cost)) as profit, sum(if(link_order_id != 0, credit_default_currency, 0 )) as linked_profit')
+            ]);
 
-
-        $chartserverdate = json_encode(array_keys($chartserver));
-
-        $chartservervalue = json_encode(array_values($chartserver));
-        $chartimeivalue = json_encode(array_values($chartimei));
-
-        //count
-        //server
-        $chartservercount = $collectserver->groupBy('completed_on')->map(function ($item) {
-            return $item->count(function ($item) {
-                return (number_format($item['profit']));
-            });
-        })
-            ->toArray();
-        //imei
-        $chartimeicount = $collectimei->groupBy('completed_on')->map(function ($item) {
-            return $item->count(function ($item) {
-                return (number_format($item['profit']));
-            });
-        })
-            ->toArray();
-        $chartcountservervalue = json_encode(array_values($chartservercount));
-        $chartcountimeivalue = json_encode(array_values($chartimeicount));
-
-        } else{
-            $chartserverdate = "";
-            $chartcountimeivalue  = "";
-            $chartcountservervalue   = "";
-            $chartimeivalue   = "";
-            $chartservervalue    = "";
-
-        }
-        return view('home', compact('chartcountservervalue', 'chartcountimeivalue', 'chartimeivalue', 'chartserverdate', 'chartservervalue', 'serveroder', 'imeioder', 'serveroderday', 'serveroderyesterday', 'serverodermonth', 'serveroderweek', 'imeioderday', 'imeioderyesterday', 'imeioderweek', 'imeiodermonth'));
+        return view('home', compact('serverchart','imeichart','serveroder','imeioder'));
     }
 }
