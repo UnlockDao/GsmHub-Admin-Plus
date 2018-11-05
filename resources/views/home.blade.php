d7b6c19ecea6e5611e683866e6e4de4d36f3ed93
ManhIT <manhit96@gmail.com>
2018-11-06 03:00:18 +0700

    Update resources/views/home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,33 +4,13 @@
         <div class="header-body">
             <!-- Card stats -->
             <div class="row">
+
                 <div class="col-xl-4 col-lg-6">
                     <div class="card card-stats mb-4 mb-xl-0">
                         <div class="card-body">
                             <div class="row">
                                 <div class="col">
-                                    <h5 class="card-title text-uppercase text-muted mb-0">Lợi nhuận Server</h5>
-                                    <span class="h2 font-weight-bold mb-0">{{$serveroder->profit}} USD</span>
-                                </div>
-                                <div class="col-auto">
-                                    <div class="icon icon-shape bg-danger text-white rounded-circle shadow">
-                                        <i class="ni ni-money-coins"></i>
-                                    </div>
-                                </div>
-                            </div>
-                            <p class="mt-3 mb-0 text-muted text-sm" style="font-weight: bold;">
-                                <span class="text-success mr-2">{{number_format($serveroder->profit*22000)}} </span>
-                                <span class="text-nowrap">VND</span>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xl-4 col-lg-6">
-                    <div class="card card-stats mb-4 mb-xl-0">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col">
-                                    <h5 class="card-title text-uppercase text-muted mb-0">Lợi nhuận IMEI</h5>
+                                    <h5 class="card-title text-uppercase text-muted mb-0">IMEI Service Profit</h5>
                                     <span class="h2 font-weight-bold mb-0">{{$imeioder->profit}} USD</span>
                                 </div>
                                 <div class="col-auto">
@@ -46,12 +26,35 @@
                         </div>
                     </div>
                 </div>
+
                 <div class="col-xl-4 col-lg-6">
                     <div class="card card-stats mb-4 mb-xl-0">
                         <div class="card-body">
                             <div class="row">
                                 <div class="col">
-                                    <h5 class="card-title text-uppercase text-muted mb-0">Tổng lợi nhuận</h5>
+                                    <h5 class="card-title text-uppercase text-muted mb-0">Server Service Profit</h5>
+                                    <span class="h2 font-weight-bold mb-0">{{$serveroder->profit}} USD</span>
+                                </div>
+                                <div class="col-auto">
+                                    <div class="icon icon-shape bg-danger text-white rounded-circle shadow">
+                                        <i class="ni ni-money-coins"></i>
+                                    </div>
+                                </div>
+                            </div>
+                            <p class="mt-3 mb-0 text-muted text-sm" style="font-weight: bold;">
+                                <span class="text-success mr-2">{{number_format($serveroder->profit*22000)}} </span>
+                                <span class="text-nowrap">VND</span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xl-4 col-lg-6">
+                    <div class="card card-stats mb-4 mb-xl-0">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col">
+                                    <h5 class="card-title text-uppercase text-muted mb-0">Total Profit</h5>
                                     <span class="h2 font-weight-bold mb-0">{{$serveroder->profit+$imeioder->profit}}
                                         USD</span>
                                 </div>
@@ -75,6 +78,15 @@
 @section('content')
     <div class="container-fluid mt--7">
         <div class="row">
+
+            <div class="col-xl-12 col-lg-12" style="padding-top: 10px">
+                <div class="card card-stats mb-4 mb-xl-0">
+                    <div class="card-body">
+                        <div id="profitchart" style="min-width: 310px; height: 300px; margin: 0 auto"></div>
+                    </div>
+                </div>
+            </div>
+
             <div class="col-xl-12 col-lg-12">
                 <div class="card card-stats mb-4 mb-xl-0">
                     <div class="card-body">
@@ -82,13 +94,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-xl-12 col-lg-12" style="padding-top: 10px">
-                <div class="card card-stats mb-4 mb-xl-0">
-                    <div class="card-body">
-                        <div id="pricing" style="min-width: 310px; height: 300px; margin: 0 auto"></div>
-                    </div>
-                </div>
-            </div>
+
             <div class="col-xl-12 col-lg-12" style="padding-top: 10px">
                 <div class="card card-stats mb-4 mb-xl-0">
                     <div class="card-body">
@@ -96,6 +102,7 @@
                     </div>
                 </div>
             </div>
+
         </div>
 
 
@@ -112,12 +119,54 @@
                 return Number(each_element.toFixed(0));
             });
 
+            Highcharts.chart('profitchart', {
+                chart: {
+                    type: 'column'
+                },
+                title: {
+                    text: 'Profit'
+                },
+                xAxis: {
+                    categories:  {!! $serverchart->pluck('date') !!}
+                },
+                yAxis: {
+                    min: 0,
+                    title: {
+                        text: ''
+                    },
+                    stackLabels: {
+                        enabled: true,
+                        style: {
+                            fontWeight: 'bold',
+                            color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+                        }
+                    }
+                },
+                colors: [
+                    '#5e72e4',
+                    '#2dce89',
+                ],
+                plotOptions: {
+                    column: {
+                        stacking: 'normal',
+                        colorByPoint: false,
+                    }
+                },
+                series: [{
+                    name: 'IMEI Service',
+                    data: imei
+                }, {
+                    name: 'Server Service',
+                    data: server
+                }]
+            });
+
             Highcharts.chart('container', {
                 chart: {
                     type: 'column'
                 },
                 title: {
-                    text: 'Total Order'
+                    text: 'Completed Orders'
                 },
                 xAxis: {
                     categories: {!! $serverchart->pluck('date') !!}
@@ -146,54 +195,11 @@
                     }
                 },
                 series: [{
-                    name: 'IMEI',
+                    name: 'IMEI Service',
                     data: {!! $imeichart->pluck('value') !!}
                 }, {
-                    name: 'Server',
+                    name: 'Server Service',
                     data: {!! $serverchart->pluck('value') !!}
-                }]
-            });
-
-
-            Highcharts.chart('pricing', {
-                chart: {
-                    type: 'column'
-                },
-                title: {
-                    text: 'Total profit'
-                },
-                xAxis: {
-                    categories:  {!! $serverchart->pluck('date') !!}
-                },
-                yAxis: {
-                    min: 0,
-                    title: {
-                        text: ''
-                    },
-                    stackLabels: {
-                        enabled: true,
-                        style: {
-                            fontWeight: 'bold',
-                            color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
-                        }
-                    }
-                },
-                colors: [
-                    '#5e72e4',
-                    '#2dce89',
-                ],
-                plotOptions: {
-                    column: {
-                        stacking: 'normal',
-                        colorByPoint: false,
-                    }
-                },
-                series: [{
-                    name: 'IMEI',
-                    data: imei
-                }, {
-                    name: 'Server',
-                    data: server
                 }]
             });
 
@@ -204,7 +210,7 @@
                     type: 'column'
                 },
                 title: {
-                    text: 'Total invoice'
+                    text: 'Income'
                 },
                 xAxis: {
                     categories:  {!! $serverchart->pluck('date') !!}
@@ -233,7 +239,7 @@
                     }
                 },
                 series: [{
-                    name: 'Invoice',
+                    name: 'Income',
                     data: invoice
                 }]
             });
