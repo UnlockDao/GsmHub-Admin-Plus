7b778566b2a2fd1ae18ffe6b158b18abddfae9ef
binhnguyen1998822 <32400888+binhnguyen1998822@users.noreply.github.com>
2018-11-09 11:15:34 +0700

    Send Maill

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -18,12 +18,12 @@ class MailController extends Controller
     {
         $to_name = 'To Nguyen';
         $to_email = 'nguyentb@s-developers.com';
-        $data = array('name' => "Sam Jose", "body" => "Test mail");
+        $data = array('name' => "Sam Jose", "body" => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
 
         Mail::send('emails.mail', $data, function ($message) use ($to_name, $to_email) {
             $message->to($to_email, $to_name)
-                ->subject('Artisans Web Testing Mail');
-            $message->from('admin@mccorp.vn', 'Test');
+                ->subject('Please Active Services');
+            $message->from('admin@mccorp.vn', 'S-Unlock');
         });
         echo 'send';
     }
