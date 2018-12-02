aa4c98a55742bbc3160be6724d8cfaf425109821
binhnguyen1998822 <32400888+binhnguyen1998822@users.noreply.github.com>
2018-12-02 10:23:24 +0700

    Fix PurchaseCost

--- app/Http/Controllers/Utility.php
+++ app/Http/Controllers/Utility.php
@@ -337,18 +337,19 @@ class Utility
                         $giatransactionfee = ($tipurchasecost * $purchasecost) / $exchangerategoc + (($purchasecost / 100) * $transactionfeegd);
                         Serverservice::where('id', $sr->id)->update(['purchase_cost' => $giatransactionfee]);
                     }
-                    if (!$serverservice->serverservicetypewiseprice->isEmpty())
-                        if ($serverservice->api_id == !null) {
-                            foreach ($serverservice->serverservicetypewiseprice as $a) {
-                                foreach ($a->apiservicetypewisepriceid as $apiserverservicetypeprice) {
-                                    if ($apiserverservicetypeprice->id == $a->api_service_type_wise_price_id) {
-                                        $purchasecost = $apiserverservicetypeprice->api_price;
-                                    }
+                }
+                if (!$serverservice->serverservicetypewiseprice->isEmpty()){
+                    if ($serverservice->api_id == !null) {
+                        foreach ($serverservice->serverservicetypewiseprice as $a) {
+                            foreach ($a->apiservicetypewisepriceid as $apiserverservicetypeprice) {
+                                if ($apiserverservicetypeprice->id == $a->api_service_type_wise_price_id) {
+                                    $purchasecost = $apiserverservicetypeprice->api_price;
                                 }
-                                $giatransactionfee = ($tipurchasecost * $purchasecost) / $exchangerategoc + (($purchasecost / 100) * $transactionfeegd);
-                                Serverservicetypewiseprice::where('id', $a->id)->update(['purchase_cost' => $giatransactionfee]);
                             }
+                            $giatransactionfee = ($tipurchasecost * $purchasecost) / $exchangerategoc + (($purchasecost / 100) * $transactionfeegd);
+                            Serverservicetypewiseprice::where('id', $a->id)->update(['purchase_cost' => $giatransactionfee]);
                         }
+                    }
                 }
             }
         }
