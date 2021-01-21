3c6a04739c4902e3ad03bf9e1ddc8083f876d13c
Mạnh Đinh Văn <manhdv@s-developers.com>
2021-01-22 06:36:56 +0700

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -19,17 +19,19 @@ class LoginController extends Controller
 
     public function postLogin(Request $request)
     {
-        $users = User::where('user_name', $request->email)->where('user_access', 'Admin')->first();
+        $col = 'user_name';
+        if ($this->checkEmail($request->email)) {
+            $col = 'email';
+        }
+        $users = User::where($col, $request->email)->where('user_access', 'Admin')->first();
         if ($users == !null) {
-            $admin = Administrator::where('user_id', $users->user_id)->where('administrator_role_id', '!=', '0')->where('role_adminplus', '1')->first();
-            if ($admin == !null) {
-                $hash = $users->bba_token;
-                $user = User::where('user_name', $request->email)
-                    ->where('password', md5($request->password . $hash))
-                    ->first();
+            $admin = Administrator::where('user_id', $users->user_id)->where('administrator_role_id', '!=',
+                '0')->where('role_adminplus', '1')->first();
 
-                if ($user == !null) {
-                    Auth::login($user);
+            if ($admin == !null) {
+                $md5passwd = md5($request->password . $users->bba_token);
+                if ($md5passwd == $users->password) {
+                    Auth::login($users);
                     return redirect('/');
                 } else {
                     return redirect('/');
@@ -41,6 +43,14 @@ class LoginController extends Controller
             return redirect('/');
         }
     }
+
+    public function checkEmail($email): bool
+    {
+        $find1 = strpos($email, '@');
+        $find2 = strpos($email, '.');
+        return ($find1 !== false && $find2 !== false && $find2 > $find1);
+    }
+
 
     public function role()
     {
@@ -55,7 +65,7 @@ class LoginController extends Controller
         return view('setting.role', compact('administrator'));
     }
 
-    public function status($par = NULL, $par2 = NULL)
+    public function status($par = null, $par2 = null)
     {
         if ($par == "status") {
             $id = $_GET['id'];
